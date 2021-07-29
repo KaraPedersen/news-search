03bef7935e442a2e10c5d28ff92048bada7dd462
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-07-29 11:54:11 -0700

    added loading to state

--- src/containers/NewsSearch.jsx
+++ src/containers/NewsSearch.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'; 
 import ArticleList from '../components/articles/ArticleList';
 import Search from '../components/search/Search';
-import { getArticles } from '../services/news-utils';
+import { getSearchedArticles, getTopHeadlines } from '../services/news-utils';
 
 export default class NewsSearch extends Component {
   state = {
     search: '',
     articles: [],
+    loading: true,
   };
+
+  async componentDidMount() {
+    const articles = await getTopHeadlines();
+
+    this.setState({ articles, loading: false });
+  }
 
   handleChange = ({ target }) => {
     this.setState({ search: target.value });
@@ -15,14 +22,18 @@ export default class NewsSearch extends Component {
 
     handleSubmit = async (e) => {
       e.preventDefault();
+      this.setState({ articles, loading: true });
+
       const articles = await
-      getArticles(this.state.search);
-      this.setState({ articles });
+      getSearchedArticles(this.state.search);
+      this.setState({ articles, loading: false });
     }
 
     render() {
       const { search, articles, loading } = this.state;
+      if(loading) return <h1> Loading...</h1>;
 
+        
       return (
         <>
           <Search
