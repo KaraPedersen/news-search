a398544422a44099afcfcc584a87f154365ba753
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-07-29 11:47:04 -0700

    removed somethings onsu=earch cuz it didnt work

--- src/services/news-utils.js
+++ src/services/news-utils.js
@@ -1,7 +1,17 @@
-export const getArticles = async (search) => {
+export const getSearchedArticles = async (search) => {
   const res = await
   // eslint-disable-next-line max-len
   fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${process.env.API_KEY}`);
+
+  const json = await res.json();
+
+  return json.articles;
+};
+
+export const getTopHeadlines = async () => {
+  const res = await
+  // eslint-disable-next-line max-len
+  fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}`);
 
   const json = await res.json();
 
